206527640af78cadbb888e81bad6a2dbdcef0554
Antonio Pegasus <Antonio@pegasus>
2025-08-24 17:49:30 +0100

    core algo implemented. now just pretty printing

--- computor.py
+++ computor.py
@@ -3,10 +3,10 @@ import sys
 from typing import *
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(order=True)
 class Monomial:
-    coefficient:    float = 0
     exponent:       float = 0
+    coefficient:    float = 0
 # print(Monomial(5).exponent, Monomial(5 ,4).exponent, Monomial(exponent=3).coefficient)
 
 import logging
@@ -34,7 +34,7 @@ def digitize(equation: str) -> list[Monomial | Literal['='] ]:
             )
         else:
             poly.append(
-                Monomial(1)
+                Monomial(0, 1)
             )
             if exprs[i] == "-":
                 debug(f"Creating negative polynomial")
@@ -52,21 +52,29 @@ def digitize(equation: str) -> list[Monomial | Literal['='] ]:
     print(poly)
     return poly
 
+#TODO: change this section ########################################
 # if len(sys.argv) < 2:
 #     exit
 
 # solutions = digitize(sys.argv[1])
 digitalForm = digitize("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
+###################################################################
 
 def additiveInvert(poly: list[Monomial]) -> list[Monomial]:
     # for monom in poly:
     #     monom.coefficient *= -1
     # return poly
-    return [Monomial(-m.coefficient, m.exponent) for m in poly]
+    return [Monomial(m.exponent, -m.coefficient) for m in poly]
 
 def simplify(poly: list[Monomial]) -> list[Monomial]:
-    for monom in poly:
-        pass
+    i: int = 0
+    while i < len(poly):
+        for m in poly:
+            if poly[i] != m and poly[i].exponent == m.exponent:
+                poly[i].coefficient += m.coefficient
+                poly.remove(m)
+        i += 1
+        
 
 def reduce(digiPoly: list[Monomial | Literal['=']]) -> list[Monomial]:
     try:
@@ -77,18 +85,33 @@ def reduce(digiPoly: list[Monomial | Literal['=']]) -> list[Monomial]:
         eq_idx = len(digiPoly)
     left = digiPoly[:eq_idx]
     right = digiPoly[eq_idx + 1:]
-    # debug(f"left: {left}")
-    # debug(f"right: {right}")
+    debug(f"left: {left}")
+    debug(f"right: {right}")
 
     right = additiveInvert(right)
     debug(f"right's additive inversion: {right}")
-    left.extend(right)
 
-    left  = simplify(left)
+    left.extend(right)
+    debug(f"before simplification: {left}")
+
+    simplify(left)
     debug(f"simplifiedLeft: {left}")
 
-    return left
+    return sorted(left)
 
 
 
 reducedForm = reduce(digitalForm)
+debug(f"reducedForm: {reducedForm}")
+
+
+
+# polyPrint(reducedForm)
+
+
+def tryInt(num: int | float) -> int | float:
+    if isinstance(num, float):
+        return int(num) if num.is_integer() else num
+    return num
+maxExp = max(reducedForm).exponent
+print("Polynomial degree:", tryInt(maxExp) )
