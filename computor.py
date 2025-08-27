9a2c6706640cfe730209f9f4cc43eadc7a5d6e0b
Antonio Pegasus <Antonio@pegasus>
2025-08-27 15:42:57 +0100

    removed some TODOs I'd already resolved

--- computor.py
+++ computor.py
@@ -28,28 +28,29 @@ def computor(equation: str):
     polyPrint(reducedForm)
 
     maxExp = tryInt(max(reducedForm).exponent)
-    print("Polynomial degree:", maxExp) #TODO: don't print if it's 0
+    if maxExp > 0:
+        print("Polynomial degree:", maxExp) #TODO: don't print if it's 0
 
     if maxExp < 0:
         raise ValueError("Polynomial degree cannot be less than 0.")
     elif maxExp < 2:
-        #TODO
         solve1dEquation(reducedForm)
     elif maxExp == 2:
-        #TODO
         solve2ndEquation(reducedForm)
     else:
         print("The polynomial degree is strictly greater than 2, I can't solve.")
     print()
 
-computor("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
-print()
-computor("5 * X^0 + 4 * X^1 = 4 * X^0")
-print()
-computor("8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0")
-print()
-computor("6 * X^0 = 6 * X^0") #TODO: gotta fix this!
-print()
-computor("10 * X^0 = 15 * X^0") #TODO: gotta fix this!
-print()
-computor("1 * X^0 + 2 * X^1 + 5 * X^2 = 0")
+computor("4 * X^2/3 - 8 * X^0 = 0")
+
+# computor("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
+# print()
+# computor("5 * X^0 + 4 * X^1 = 4 * X^0")
+# print()
+# computor("8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0")
+# print()
+# computor("6 * X^0 = 6 * X^0")
+# print()
+# computor("10 * X^0 = 15 * X^0")
+# print()
+# computor("1 * X^0 + 2 * X^1 + 5 * X^2 = 0")
